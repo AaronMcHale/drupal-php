10d9c015037c048f01164eeded0001f7deae7b25
Aaron McHale <aaron@mchale.me>
2023-11-19 17:50:43 +0000

    Switch from ssh to https Git URLs for libraries to allow verify CI job to pass

--- download-libs.sh
+++ download-libs.sh
@@ -9,24 +9,30 @@ cd lib
 
 # Clone PHP upstream if it doesn't exist
 if [ ! -d php ]; then
-    git clone git@github.com:docker-library/php.git
+    echo "Downloading docker-library/php..."
+    git clone https://github.com/docker-library/php.git
 fi
 cd php && git fetch
 if [ -f ../.php-known-hash ]; then
+    echo "Checking out known version of docker-library/php..."
     git checkout -d "$(cat ../.php-known-hash)"
 else
+    echo "Known version for docker-library/php not stored, storing latest commit as known version..."
     echo "$(git rev-parse HEAD)" > ../.php-known-hash
 fi
 cd ..
 
 # Clone bashbrew if it doesn't exist
 if [ ! -d bashbrew ]; then
-    git clone git@github.com:docker-library/bashbrew.git
+    echo "Downloading docker-library/bashbrew..."
+    git clone https://github.com/docker-library/bashbrew.git
 fi
 cd bashbrew && git fetch
 if [ -f ../.bashbrew-known-hash ]; then
+    echo "Checking out known version of docker-library/bashbrew..."
     git checkout -d "$(cat ../.bashbrew-known-hash)"
 else
+    echo "Known version for docker-library/bashbrew not stored, storing latest commit as known version..."
     echo "$(git rev-parse HEAD)" > ../.bashbrew-known-hash
 fi
 cd ..
--- update.sh
+++ update.sh
@@ -12,14 +12,23 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 ./download-libs.sh
 
 # Update the known version of PHP to the latest version
+echo "Starting update of libraries..."
 cd lib/php
-git remote set-url origin git@github.com:docker-library/php.git
+echo "Setting remote for docker-library/php..."
+git remote set-url origin https://github.com/docker-library/php.git
+echo "Pulling latest commits and checking out master for docker-library/php..."
 git pull origin master && git checkout master
+echo "Storing latest commit as known version for docker-library/php..."
 echo "$(git rev-parse HEAD)" > ../.php-known-hash
+
 cd ../bashbrew
-git remote set-url origin git@github.com:docker-library/bashbrew.git
+echo "Setting remote for docker-library/bashbrew..."
+git remote set-url origin https://github.com/docker-library/bashbrew.git
+echo "Pulling latest commits and checking out master for docker-library/bashbrew..."
 git pull origin master && git checkout master
+echo "Storing latest commit as known version for docker-library/bashbrew..."
 echo "$(git rev-parse HEAD)" > ../.bashbrew-known-hash
 cd ../..
+echo "Library update complete."
 
 ./apply-templates.sh "$@"
