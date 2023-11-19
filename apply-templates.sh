2fe539c2f60bb06e5dd0073444c0cf9e1a16489e
Aaron McHale <aaron@mchale.me>
2023-11-19 15:48:00 +0000

    Add simple tempalte Dockerfile, apply-template.sh script and generate from template

---
+++ apply-templates.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+# Get the versions.json from lib/php
+if [ ! -f versions.json ]; then
+	cp lib/php/versions.json .
+fi
+
+jqt='.jq-template.awk'
+if [ -n "${BASHBREW_SCRIPTS:-}" ]; then
+	jqt="$BASHBREW_SCRIPTS/jq-template.awk"
+elif [ "$BASH_SOURCE" -nt "$jqt" ]; then
+	# https://github.com/docker-library/bashbrew/blob/master/scripts/jq-template.awk
+	wget -qO "$jqt" 'https://github.com/docker-library/bashbrew/raw/9f6a35772ac863a0241f147c820354e4008edf38/scripts/jq-template.awk'
+fi
+
+if [ "$#" -eq 0 ]; then
+	versions="$(jq -r 'keys | map(@sh) | join(" ")' versions.json)"
+	eval "set -- $versions"
+fi
+
+generated_warning() {
+	cat <<-EOH
+		#
+		# NOTE: THIS DOCKERFILE IS GENERATED VIA "apply-templates.sh"
+		#
+		# PLEASE DO NOT EDIT IT DIRECTLY.
+		#
+
+	EOH
+}
+
+for version; do
+	export version
+
+	rm -rf "$version"
+
+	if jq -e '.[env.version] | not' versions.json > /dev/null; then
+		echo "deleting $version ..."
+		continue
+	fi
+
+	variants="$(jq -r '.[env.version].variants | map(@sh) | join(" ")' versions.json)"
+	eval "variants=( $variants )"
+
+	for dir in "${variants[@]}"; do
+		suite="$(dirname "$dir")" # "buster", etc
+		variant="$(basename "$dir")" # "cli", etc
+		export suite variant
+
+		# our apply-templates.sh is simplier than the one in lib/php
+		# Our "from" line is always the php image, and we use the exact
+		# same version, variant and suite, so we don't need logic to
+		# figure out the version number, etc.
+        from="php:$version-$variant-$suite"
+		export from
+		# apply-templates.sh in lib/php also varies the cmd depnding on
+		# the variant, but we just use the cmd from the php image, so we
+		# don't need to set it here.
+
+		echo "processing $version/$dir ..."
+		mkdir -p "$version/$dir"
+
+		{
+			generated_warning
+			gawk -f "$jqt" 'Dockerfile-linux.template'
+		} > "$version/$dir/Dockerfile"
+	done
+done
