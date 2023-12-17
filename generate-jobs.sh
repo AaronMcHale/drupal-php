53929060bff3bb15aad184b1747472bc8741304c
Aaron McHale <AaronMcHale@users.noreply.github.com>
2023-12-17 14:55:23 +0000

    Add multi-platform support and restructure CI job generation (#3)
    * generate-jobs.sh
    
    * Use generate-jobs.sh and test multi-platform build
    
    * Download libraries for generate-jobs
    
    * Generate-jobs still needs bashbrew
    
    * Test filtering out arm32v5
    
    * Specify command to run for test-image
    
    * Transform arches to add linux prefix
    
    * Don't need to download bashbrew sources anymore
    
    * Transform v to /v in arches
    
    * Download jq-template.awk from bashbrew
    
    * Fix apply-template.sh
    
    * Repalce "arm32" with "arm" in arches
    
    * Remove redundant test CI steps, move multi-platform build to main branch CI
    
    * Delete version-ci-json.lock no longer needed

---
+++ generate-jobs.sh
@@ -0,0 +1,277 @@
+#!/usr/bin/env bash
+#
+# Generates the matrix used for the generate-jobs GitHuab CI step
+
+set -Eeuo pipefail
+
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+# Helper function to remove all whitespaces from a line.
+transform_line() {
+    line="$1"
+    echo "${line//[[:blank:]]/}"
+}
+
+# Helper function to add the repository name to each tag that
+# generate-stackbrew-library.sh returns.
+transform_tags() {
+    if [ -z "$1" ]; then
+        echo 'call to transform_tags() resulted in error: $1 must not be emtpy, argument 1 is required.' >&1
+        exit 1
+    fi
+    input="$1"
+    # we need to add an extra comma to the end otherwise the last value may
+    # not be included
+    if [ "${input:-1}" != ',' ]; then
+        input="$input"','
+    fi
+    tags="$(
+        while read -d ',' -r tag; do
+            if [ ! -z "$tag" ]; then
+                printf "%s:%s," "aaronmchale/drupal-php" "$tag"
+            fi
+        done <<< "$input"
+    )"
+    printf "${tags:0:-1}"
+}
+
+# Transform format of arches
+transform_arches() {
+    if [ -z "$1" ]; then
+        echo 'call to transform_arches() resulted in error: $1 must not be emtpy, argument 1 is required.' >&1
+        exit 1
+    fi
+    input="$1"
+    # we need to add an extra comma to the end otherwise the last value may
+    # not be included
+    if [ "${input:-1}" != ',' ]; then
+        input="$input"','
+    fi
+    platform='linux'
+    arches="$(
+        while read -d ',' -r arch; do
+            if [ ! -z "$arch" ]; then
+                printf "%s/%s," "$platform" "$arch"
+            fi
+        done <<< "$input"
+    )"
+    # We need to transform "v" in some arches to be "/v", so that,
+    # for instance, "arm32v6" becomes "arm32/v6", but only when "v" is
+    # followed by numbers. This helps avoids any accidental replacement when
+    # "v" may appear in the architecture name somewhere else.
+    # Find: `v\([0-9]\+\)`, `v` followed by any number;
+    # Replacement: `\/v\1`, `v` followed by the original number (`\1` is the
+    # matched number).
+    # "arm32" needs to be replaced with just "arm"
+    arches="$(
+        printf "$arches" | sed \
+            -e 's/v\([0-9]\+\)/\/v\1/g' \
+            -e 's/arm32/arm/g'
+    )"
+    printf "${arches:0:-1}"
+}
+
+# helper function to output test error messages
+# `$1`: name of function
+# `$2`: input sent to function
+# `$3`: expected output from function
+# `$4`: actual output from function
+exit_with_test_error() {
+    echo "Error when testing ""$1"", input did not match expected output:" >&1
+    echo >&1
+    echo "  input: ""$2" >&1
+    echo >&1
+    echo "  expected: ""$3" >&1
+    echo >&1
+    echo "  actual output: ""$4" >&1
+    echo >&1
+    exit 1
+}
+
+# Tests for `transform_line()` function to ensure it's working as expected
+test_transform_line() {
+    # Test empty line
+    input=""
+    expected=""
+    got="$(transform_line "$input")"
+    if [ "$got" != "$expected" ]; then
+        exit_with_test_error "transform_line" "$input" "$expected" "$got"
+    fi
+
+    # Test line with one value (and forward slashes)
+    input="Directory: 8.3-rc/bookworm/cli"
+    expected="Directory:8.3-rc/bookworm/cli"
+    got="$(transform_line "$input")"
+    if [ "$got" != "$expected" ]; then
+        exit_with_test_error "transform_line" "$input" "$expected" "$got"
+    fi
+
+    # Test line with multiple values
+    input="Architectures: amd64, arm32v5, arm32v7, arm64v8, i386, mips64le, ppc64le, s390x"
+    expected="Architectures:amd64,arm32v5,arm32v7,arm64v8,i386,mips64le,ppc64le,s390x"
+    got="$(transform_line "$input")"
+    if [ "$got" != "$expected" ]; then
+        exit_with_test_error "transform_line" "$input" "$expected" "$got"
+    fi
+}
+test_transform_line
+
+# Tests for `transform_tags()` function to ensure it's working as expected
+test_transform_tags() {
+    # Test single value
+    input="8"
+    expected="aaronmchale/drupal-php:8"
+    got="$(transform_tags "$input")"
+    if [ "$got" != "$expected" ]; then
+        exit_with_test_error "transform_tags" "$input" "$expected" "$got"
+    fi
+
+    # Test adding comma to end of string
+    input="8,"
+    expected="aaronmchale/drupal-php:8"
+    got="$(transform_tags "$input")"
+    if [ "$got" != "$expected" ]; then
+        exit_with_test_error "transform_tags" "$input" "$expected" "$got"
+    fi
+
+    # Test version number and "latest"
+    input="8,latest"
+    expected="aaronmchale/drupal-php:8,aaronmchale/drupal-php:latest"
+    got="$(transform_tags "$input")"
+    if [ "$got" != "$expected" ]; then
+        exit_with_test_error "transform_tags" "$input" "$expected" "$got"
+    fi
+
+    # test a real string, probably the longest one, with final tag as "latest"
+    input="8.2.12-cli-bookworm,8.2-cli-bookworm,8-cli-bookworm,cli-bookworm,8.2.12-bookworm,8.2-bookworm,8-bookworm,bookworm,8.2.12-cli,8.2-cli,8-cli,cli,8.2.12,8.2,8,latest"
+    expected="aaronmchale/drupal-php:8.2.12-cli-bookworm,aaronmchale/drupal-php:8.2-cli-bookworm,aaronmchale/drupal-php:8-cli-bookworm,aaronmchale/drupal-php:cli-bookworm,aaronmchale/drupal-php:8.2.12-bookworm,aaronmchale/drupal-php:8.2-bookworm,aaronmchale/drupal-php:8-bookworm,aaronmchale/drupal-php:bookworm,aaronmchale/drupal-php:8.2.12-cli,aaronmchale/drupal-php:8.2-cli,aaronmchale/drupal-php:8-cli,aaronmchale/drupal-php:cli,aaronmchale/drupal-php:8.2.12,aaronmchale/drupal-php:8.2,aaronmchale/drupal-php:8,aaronmchale/drupal-php:latest"
+    got="$(transform_tags "$input")"
+    if [ "$got" != "$expected" ]; then
+        exit_with_test_error "transform_tags" "$input" "$expected" "$got"
+    fi
+}
+test_transform_tags
+
+# Tests for `transform_arches()` function to ensure it's working as expected
+test_transform_arches() {
+    # Test single value, platform added
+    input="amd64"
+    expected="linux/amd64"
+    got="$(transform_arches "$input")"
+    if [ "$got" != "$expected" ]; then
+        exit_with_test_error "transform_arches" "$input" "$expected" "$got"
+    fi
+
+    # Test adding comma to end of string
+    input="amd64,"
+    expected="linux/amd64"
+    got="$(transform_arches "$input")"
+    if [ "$got" != "$expected" ]; then
+        exit_with_test_error "transform_arches" "$input" "$expected" "$got"
+    fi
+
+    # Test single value, with version (lower "v")
+    input="arm64v8"
+    expected="linux/arm64/v8"
+    got="$(transform_arches "$input")"
+    if [ "$got" != "$expected" ]; then
+        exit_with_test_error "transform_arches" "$input" "$expected" "$got"
+    fi
+
+    # Test replacing "arm32" with "arm"
+    input="arm32"
+    expected="linux/arm"
+    got="$(transform_arches "$input")"
+    if [ "$got" != "$expected" ]; then
+        exit_with_test_error "transform_arches" "$input" "$expected" "$got"
+    fi
+
+    # Test real string
+    input="amd64,arm32v5,arm32v7,arm64v8,i386,mips64le,ppc64le,s390x"
+    expected="linux/amd64,linux/arm/v5,linux/arm/v7,linux/arm64/v8,linux/i386,linux/mips64le,linux/ppc64le,linux/s390x"
+    got="$(transform_arches "$input")"
+    if [ "$got" != "$expected" ]; then
+        exit_with_test_error "transform_arches" "$input" "$expected" "$got"
+    fi
+}
+test_transform_arches
+
+if [ ! -f lib/php/generate-stackbrew-library.sh ]; then
+	echo "Cannot find library files to copy from, run ./download-libs.sh"
+	exit 1
+fi
+
+# Use the output of `generate-stackbrew-library.sh` from php upstream
+cd lib/php
+
+# Initialise empty variables
+declare tags= arches= dir= name=
+
+# generate-stackbrew-library.sh generates an output in the follwing format
+# ```
+# Tags: 8.3.0RC6-cli-bookworm, 8.3-rc-cli-bookworm, 8.3.0RC6-bookworm, 8.3-rc-bookworm, 8.3.0RC6-cli, 8.3-rc-cli, 8.3.0RC6, 8.3-rc
+# Architectures: amd64, arm32v5, arm32v7, arm64v8, i386, mips64le, ppc64le, s390x
+# GitCommit: bde3d5c08a72f61ed5af7592b1910faed1e4a3f2
+# Directory: 8.3-rc/bookworm/cli
+#
+# Tags: 8.3.0RC6-apache-bookworm, 8.3-rc-apache-bookworm, 8.3.0RC6-apache, 8.3-rc-apache
+# Architectures: amd64, arm32v5, arm32v7, arm64v8, i386, mips64le, ppc64le, s390x
+# GitCommit: bde3d5c08a72f61ed5af7592b1910faed1e4a3f2
+# Directory: 8.3-rc/bookworm/apache
+#
+# ```
+# And so on...
+# It includes every version/variation, along with all of the tags and platforms that
+# we need.
+strategy="$(
+set -Eeuo pipefail
+./generate-stackbrew-library.sh | while IFS=$'\n' read -r line; do
+    # Remove all whitespace characters (spaces, tabs, etc) from the line
+    # so that, for instance, the "Tags" line looks like:
+    # `Tags:8.3.0RC6-apache-bookworm,8.3-rc-apache-bookworm,8.3.0RC6-apache, 8.3-rc-apache`
+    line="$(transform_line "$line")"
+
+    # Split the line into the key and value, where for instnace Tags is the key and
+    # everything after `:` is the avlue
+    IFS=$':' read -r key value <<< "$line"
+
+    case "$key" in
+        "Tags")
+            tags="$(transform_tags "$value")" ;;
+        "Architectures")
+            arches="$(transform_arches "$value")" ;;
+        "Directory")
+            dir="$value"
+            # For the name of this job, we use the value of `$dir` but
+            # replace the `/` directory separateor with `-`.
+            name="${dir//\//-}"
+            ;;
+    esac
+
+    if [ -z "$line" ] && [ -n "$tags" ] && [ -n "$arches" ] && [ -n "$dir" ]; then
+        # If we get here, we've reached a empty line, and we have values for
+        # `$tags`, `$arches` and `$dir`, so we're ready to construct the JSON
+        # object for this job.
+        echo "{
+            \"name\": \"$name\",
+            \"dir\": \"$dir\",
+            \"tags\": \"$tags\",
+            \"arches\": \"$arches\"
+        }"
+        # Reset variables to empty strings, the next line should be the start
+        # of a new version/variant.
+        declare tags= arches= dir= name=
+    fi
+done | jq -cs '
+		{
+			"fail-fast": false,
+			matrix: { include: . },
+		}
+	'
+)"
+
+if [ -t 1 ]; then
+	jq <<<"$strategy"
+else
+	cat <<<"$strategy"
+fi
